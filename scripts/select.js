d4150da60c91291b8370fd8480053195a82870fd
Isa Delatore <85897897+IsaDelatore@users.noreply.github.com>
2022-05-17 08:48:09 -0300

    Update select.js
    Now it shows the pokemon image

--- scripts/select.js
+++ scripts/select.js
@@ -1,19 +1,35 @@
-d3.csv("https://gist.githubusercontent.com/armgilles/194bcff35001e7eb53a2a8b441e8b2c6/raw/92200bc0a673d5ce2110aaad4544ed6c4010f687/pokemon.csv", function(error, data) {
-  var select = d3.select(".container")
-  .append("div")
+
+d3.csv("https://raw.githubusercontent.com/IsaDelatore/PokemonRPG/main/pokemon.csv", function(error, data) {
+  var select = d3.select(".dropbox")
   .append("select")
 
   select
   .on("change", function(d) {
-    var name = d3.select(this).property("Name");
-    alert(name);
+
+    //Getting pokedex code and showing pokemon image
+    var test = document.querySelector('select')
+    var index = test.options[test.selectedIndex].getAttribute("index")
+    var imagem = document.getElementById("image");
+
+    if (imagem.hasChildNodes()) {
+      var img = document.getElementById('aaa');
+      img.parentNode.removeChild(img);
+    }
+
+    var img = document.createElement("img");
+    img.setAttribute("id", "aaa")
+    img.src = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/" + data[index]["Number"] + ".png"
+    imagem.appendChild(img);
+
+
+
   });
 
 select.selectAll("option")
 .data(data)
 .enter()
   .append("option")
-  .attr("Name", function (d) { return d.Name; })
-  .text(function (d) { return d.Name; });
+  .attr("index", function (d) { return d.Index; })//atributo
+  .text(function (d) { return d.Name; });//label
 
 });
