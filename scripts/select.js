5e36bf6316888199c3af516035a0d3c22f855195
Isa Delatore <85897897+IsaDelatore@users.noreply.github.com>
2022-05-20 19:19:19 -0300

    Update select.js
    Did it in a smart way

--- scripts/select.js
+++ scripts/select.js
@@ -1,10 +1,15 @@
+const poke_stats = ["Type","Total","HP","Attack","Defense","Sp-Atk","Sp-Def","Speed","Egg-Group","Experience-type"];
+const tormenta_stats = ["HP-base","HP-Nv","FOR","DES","CON","INT","SAB","CAR","Armor"];
+
 
 d3.csv("https://raw.githubusercontent.com/IsaDelatore/PokemonRPG/main/pokemon.csv", function(error, data) {
   var select = d3.select(".dropbox")
   .append("select")
 
+
   select
   .on("change", function(d) {
+
 
     //Getting pokedex code and showing pokemon image, pokemon attributes and rpg attributes
     var test = document.querySelector('select')
@@ -16,27 +21,10 @@ d3.csv("https://raw.githubusercontent.com/IsaDelatore/PokemonRPG/main/pokemon.cs
       var img = document.getElementById('pokemon-image');
       img.parentNode.removeChild(img);
 
-      var type = document.getElementById("type");
-      type.parentNode.removeChild(type);
-      var total = document.getElementById("total");
-      total.parentNode.removeChild(total);
-      var hp = document.getElementById("hp");
-      hp.parentNode.removeChild(hp);
-      var attack = document.getElementById("attack");
-      attack.parentNode.removeChild(attack);
-      var defense = document.getElementById("defense");
-      defense.parentNode.removeChild(defense);
-      var spatk = document.getElementById("spatk");
-      spatk.parentNode.removeChild(spatk);
-      var spdef = document.getElementById("spdef");
-      spdef.parentNode.removeChild(spdef);
-      var speed = document.getElementById("speed");
-      speed.parentNode.removeChild(speed);
-      var egg = document.getElementById("egg");
-      egg.parentNode.removeChild(egg);
-      var xptp = document.getElementById("xptp");
-      xptp.parentNode.removeChild(xptp);
-
+      for(i in poke_stats){
+        let paragrafo = document.getElementById(poke_stats[i]);
+        paragrafo.parentNode.removeChild(paragrafo)
+      }
     }
 
     var img = document.createElement("img");
@@ -44,59 +32,23 @@ d3.csv("https://raw.githubusercontent.com/IsaDelatore/PokemonRPG/main/pokemon.cs
     img.src = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/" + data[index]["Number"] + ".png"
     imagem.appendChild(img);
 
-    var ptype = document.createElement("p");
-    ptype.setAttribute("id", "type");
-    var ptotal = document.createElement("p");
-    ptotal.setAttribute("id", "total");
-    var php = document.createElement("p");
-    php.setAttribute("id", "hp");
-    var pattack = document.createElement("p");
-    pattack.setAttribute("id", "attack");
-    var pdefense = document.createElement("p");
-    pdefense.setAttribute("id", "defense");
-    var pspatk = document.createElement("p");
-    pspatk.setAttribute("id", "spatk");
-    var pspdef = document.createElement("p");
-    pspdef.setAttribute("id", "spdef");
-    var pspeed = document.createElement("p");
-    pspeed.setAttribute("id", "speed");
-    var pegg = document.createElement("p");
-    pegg.setAttribute("id", "egg");
-    var pxptp = document.createElement("p");
-    pxptp.setAttribute("id", "xptp");
+    for(i in poke_stats){
+      let paragrafo = document.createElement("p");
+      paragrafo.setAttribute("id", poke_stats[i]);
+      let dado
+      if(poke_stats[i] == "Type"){
+        dado = document.createTextNode(data[index]["Type-1"] + "/" + data[index]["Type-2"]);
+      } else if (poke_stats[i] == "Egg-Group"){
+        dado = document.createTextNode(data[index]["Egg-Group-1"] + "/" + data[index]["Egg-Group-2"]);
+      } else {
+        dado = document.createTextNode(data[index][poke_stats[i]]);
+      }
+      paragrafo.appendChild(dado);
+      pstats.appendChild(paragrafo);
 
-    var type = document.createTextNode(data[index]["Type-1"] + "/" + data[index]["Type-2"]);
-    var total = document.createTextNode(data[index]["Total"]);
-    var hp = document.createTextNode(data[index]["HP"]);
-    var attack = document.createTextNode(data[index]["Attack"]);
-    var defense = document.createTextNode(data[index]["Defense"]);
-    var spatk = document.createTextNode(data[index]["Sp-Atk"]);
-    var spdef = document.createTextNode(data[index]["Sp-Def"]);
-    var speed = document.createTextNode(data[index]["Speed"]);
-    var egg = document.createTextNode(data[index]["Egg-Group-1"] + "/" + data[index]["Egg-Group-2"]);
-    var xptp = document.createTextNode(data[index]["Experience-type"]);
+    }
 
-    ptype.appendChild(type);
-    ptotal.appendChild(total);
-    php.appendChild(hp);
-    pattack.appendChild(attack);
-    pdefense.appendChild(defense);
-    pspatk.appendChild(spatk);
-    pspdef.appendChild(spdef);
-    pspeed.appendChild(speed);
-    pegg.appendChild(egg);
-    pxptp.appendChild(xptp);
 
-    pstats.appendChild(ptype);
-    pstats.appendChild(ptotal);
-    pstats.appendChild(php);
-    pstats.appendChild(pattack);
-    pstats.appendChild(pdefense);
-    pstats.appendChild(pspatk);
-    pstats.appendChild(pspdef);
-    pstats.appendChild(pspeed);
-    pstats.appendChild(pegg);
-    pstats.appendChild(pxptp);
 
   });
 
